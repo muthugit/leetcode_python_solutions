6880b9547a9c72d9bca257900bbdaf765c1fd51a
Muthupandian <contact@muthupandian.in>
2023-08-07 11:28:00 +0530

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if nums[i] + nums[i+1] == target:
+                return [i, i+1]
